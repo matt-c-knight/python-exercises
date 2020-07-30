97f125ee974e4ace486f278dc073f92c3101764a
Matthew Knight <mattknight.sa@gmail.com>
2020-07-30 11:33:47 -0500

    first commit

---
+++ data-types.py
@@ -0,0 +1,42 @@
+# Identify the data type of the following values:
+
+# 99.9
+# float
+# "False"
+# string
+# False
+# boolean
+# '0'
+# string
+# 0
+# integar
+# True
+# Boolean
+# 'True'
+# String
+# [{}]
+# list
+# {'a': []}
+# dict
+
+# What data type would best represent:
+
+# A term or phrase typed into a search box?
+# string
+# If a user is logged in?
+# boolean
+# A discount amount to apply to a user's shopping cart?
+# float
+# Whether or not a coupon code is valid?
+# boolean
+# An email address typed into a registration form?
+# string
+# The price of a product?
+# float
+# A Matrix?
+# list
+# The email addresses collected from a registration form?
+# list
+# Information about applicants to Codeup's data science program?
+# dict
+
