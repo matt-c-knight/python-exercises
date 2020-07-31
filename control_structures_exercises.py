2a6a9b14f5f033da916263f3f17640ceb75bc6e4
Matthew Knight <mattknight.sa@gmail.com>
2020-07-31 14:52:37 -0500

    finished work question

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -13,8 +13,18 @@ if answer_two == "Monday" or answer_two == "Tuesday" or answer_two == "Wednesday
 else:
     print("It's the weekend!")
 # create variables and make up values for
-
 # the number of hours worked in one week
+hours_worked = int(input("How many hours did you work?"))
 # the hourly rate
+rate = 14
 # how much the week's paycheck will be
+# paycheck = hours_worked * rate
 # write the python code that calculates the weekly paycheck. You get paid time and a half if you work more than 40 hours
+if hours_worked > 40:
+   overtime = hours_worked - 40
+   overtime_total = overtime * (rate * 1.5)
+   paycheck_total = overtime_total + ((hours_worked - overtime) * rate)
+   print(paycheck_total)
+else:
+   check = hours_worked * rate
+   print(check)
