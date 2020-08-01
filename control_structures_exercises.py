21c6d2271cf6a1d2378f1b5a67e936062efe2930
Matthew Knight <mattknight.sa@gmail.com>
2020-08-01 13:23:55 -0500

    working on #2

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -28,3 +28,23 @@ if hours_worked > 40:
 else:
    check = hours_worked * rate
    print(check)
+
+
+# Create an integer variable i with a value of 5.
+i = 5
+# Create a while loop that runs so long as i is less than or equal to 15
+# Each loop iteration, output the current value of i, then increment i by one.
+
+while i <= 15:
+    print(i)
+    i += 1
+
+# Create a while loop that will count by 2's starting with 0 and ending at 100. 
+n = 0
+while n < 100:
+    print(n)
+    n += 2
+# Follow each number with a new line.
+# Alter your loop count backwards by 5's from 100 to -10.
+# Create a while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. 
+# Output should equal:
