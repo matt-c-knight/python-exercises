bf96222e45c26f794392e071af110731c5f603db
Matthew Knight <mattknight.sa@gmail.com>
2020-08-04 16:13:20 -0500

    added pseudo code

--- function_exercises.py
+++ function_exercises.py
@@ -6,6 +6,7 @@
 # is either the number or the string 2, False otherwise.
 def is_two(x):
     assert type(x) == int or type(x) == str, "Invalid Input"
+    # Checking two conditions, if param is the number 2 or a string val of 2
     if x == 2 or x == '2':
         return True
     else:
@@ -16,7 +17,9 @@ def is_two(x):
 # passed string is a vowel, False otherwise.
 def is_vowel(letter):
     assert type(letter) == str, "Invalid Input"
+    # convert param to lower in order to have less to iterate thru in list
     letter = letter.lower()
+    # if param is in list then evaluates to true
     if letter in ['a', 'e', 'i', 'o', 'u']:
         return True
     else:
@@ -27,6 +30,7 @@ def is_vowel(letter):
 # function to accomplish this.
 def is_consonant(letter):
     assert type(letter) == str, "Invalid Input"
+    # using previous function in order to not be redundant. Instead of vowel being true, would eval to false
     if is_vowel(letter) == True:
         return False
     else:
@@ -37,6 +41,7 @@ def is_consonant(letter):
 # the word starts with a consonant.
 def cap_word(word):
     assert type(word) == str, "Invalid Input"
+    # using previous function to check for first char of string. Then capitalizing first char.
     if is_consonant(word[0]) == True:
         return word.capitalize()
     else:
@@ -47,6 +52,7 @@ def cap_word(word):
 # and the bill total, and return the amount to tip.
 def calculate_tip(tip,total):
     assert type(tip) == float and type(total) == float or type(total) == int, "Invalid Input"
+    # multiplying tip in decimal form by total, then rounding it to two decimal places
     amount = tip * total
     return round(amount, 2)
 
@@ -56,11 +62,12 @@ calculate_tip(.3, 40)
 # and return the price after the discount is applied.
 def apply_discount(price,discount):
     assert type(price) == float or type(price) == int and type(discount) == float, "Invalid Input"
+#    find discount amt by mult price by discount percent, then subtract discount from price
     discount_amt = price * discount
     total = price - discount_amt
     return total
 
-apply_discount(25,.10)
+# apply_discount(25,.10)
 
 # Define a function named handle_commas. 
 # It should accept a string that is a number that contains commas 
@@ -138,3 +145,14 @@ def cumulative_sum(seq):
         i += x
         new_list.append(i)
     return new_list
+
+# __________________________________________________
+# Bonus
+# Create a function named twelveto24. It should accept a string in the 
+# format 10:45am or 4:30pm and return a string that is the representation 
+# of the time in a 24-hour format.
+def twelveto24(word):
+    tod = word[-2:]
+    if tod == "am":
+        if 
+    
