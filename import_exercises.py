8a7eb4004aac9087f85087b17e92401b67ee06b4
Matthew Knight <mattknight.sa@gmail.com>
2020-08-05 11:53:27 -0500

    first commit

---
+++ import_exercises.py
@@ -0,0 +1,39 @@
+# Import and test 3 of the functions from your functions exercise file.
+
+# Import each function in a different way:
+
+# import the module and refer to the function with the . syntax
+# use from to import the function directly
+# use from and give the function a different name
+
+import function_exercises as fn
+from function_exercises import is_consonant
+from function_exercises import handle_commas as hc
+fn.is_vowel("a")
+fn.is_consonant("n")
+hc("1,000")
+
+# How many different ways can you combine the letters 
+# from "abc" with the numbers 1, 2, and 3?
+list_1 = [a,b,c]
+list_2 = [1,2,3]
+import itertools
+# combos = itertools.permutations(list_1, len(list_2))
+# print(combos)
+# unique = []
+# for comb in combos:
+#     zipped = zip(comb, list_2)
+#     unique.append(list(zipped))
+
+list_3 = list(itertools.product(list_1,list_2))
+print(list_3)
+print(len(list_3))
+# How many different ways can you combine 
+# two of the letters from "abcd"?
+val = "abcd"
+unique = combinations(val, 2)
+final = [' '.join(i) for i in unique]
+print(final)
+
+perm = list(permutations("abcd", 2))
+print(len(perm))
